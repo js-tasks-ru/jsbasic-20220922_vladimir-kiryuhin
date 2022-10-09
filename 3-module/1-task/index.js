c90e2ea834b11db6e208651f96da3447dbdf3883
Vladimir <vova11999977@gmail.com>
2022-10-09 12:39:14 +0300

    3rd module 1st task

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,10 @@
 function namify(users) {
-  // ваш код...
+let names = [];
+for (let i = 0; i < users.length; i++) {
+let user = users[i];
+  if (user.age === 25, 30, 28) {
+    names.push(`${user.name}`);
+  }
+}
+  return names;
 }
